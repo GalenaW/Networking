7561c2a062e50d550064394628c91348091b6a61
Galena <galena.v.wilson@gmail.com>
2018-09-19 18:53:29 -0800

    fixed bugs

--- Server/TFTPserver.py
+++ Server/TFTPserver.py
@@ -1,86 +1,88 @@
 #****************************************************************
-# 
-#
-#
-#
+# TFTPServer.py
+# Author: Galena Wilson
+# Purpose:
 #
 #****************************************************************
+
 from socket import *
 import packets
 import os
 import ntpath
-    
+
 def extract_request(pac):
-    string = pac.decode()
-    end_pos = string.index("\x00\x00", 0, len(string)-1)
-    
-    return int.from_bytes(pac[0:2], byteorder = "big"), pac[2:end_pos]
-    
+    end_pos = pac[10:].find(b"\x00")
+    filename = pac[10:10+end_pos].decode()
+    return int.from_bytes(pac[8:10], byteorder = "big"), filename
+
+
 def to_int(binary):
-    return int.from_bytes(binary, byteorder = "big")
-    
+    return int.from_bytes(binary, byteorder="big")
+
+
 def get_fname(file):
     head, tail = ntpath.split(file)
-    return tail or ntpath.basename(head)   
-    
-serverPort = 12000
-serverSocket =  socket(AF_INET, SOCK_STREAM)
+    return tail or ntpath.basename(head)
+
+
+serverPort = 12018
+serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
-print('The server is ready do receive on port 12000')
 
-    
+print('The server is ready do receive.')
+
 while True:
     connectionSocket, addr = serverSocket.accept()
     packet = connectionSocket.recv(1024)
+   
     opcode, filename = extract_request(packet)
-    
-    
-    #if a RRQ is initiated:
+    # if a RRQ is initiated:
     if opcode == 1:
-        #read data from file
-        
+        # read data from file
+
         with open(filename, 'rb') as file:
-        
+
             # send data package with block # 1
             file_s = os.path.getsize(filename)
-            end_file = round(file_s/512)
+            end_file = round(file_s / 512)
             
+            # in case of a small file:
             if end_file == 0:
                 end_file = 1
-            
-            data = file.read(512)               # read data
-            pack = packets.DATA_packet(data, 1) # create data pack
-            connectionSocket.send(pack)   
-            ack = connectionSocket.recv(4) 
-            
-            for i in range (2, end_file+1):             # if the previous packet is ackn
-                if to_int(ack[2:]) == i-1:
-                    data = file.read(512)               # read data
-                    pack = packets.DATA_packet(data, i) # create data pack
-                    connectionSocket.send(pack)   
-                ack = connectionSocket.recv(4)  
-            file.close()
-                    
-        # TO DO:
-        # if error is sent - resend the last data package?
         
+            for i in range(1, end_file + 1):            # if the previous packet is ackn
+                data = file.read(512)                   # read data
+                pack = packets.DATA_packet(data, i)     # create data pack
+                dgram = packets.datagramHeader(12001, 12000, pack)
+                connectionSocket.send(dgram)
+                ack = connectionSocket.recv(12)
+                
+            file.close()
+
+        # TO DO:
+
     # if a WRQ request is initiated
     if opcode == 2:
-        #send acknowledgement with block number 0
-        ack_pack=packets.ACK_packet((0).to_bytes(2, byteorder = "big"))
-        connectionSocket.send(ack_pack)
+        # send acknowledgement with block number 0
+        ack_pack = packets.ACK_packet((0).to_bytes(2, byteorder="big"))
+        dgram = packets.datagramHeader(12000, 12001, ack_pack)
+        
+        connectionSocket.send(dgram)
         # receive data package
+        filename = "copy_" + filename 
         with open(get_fname(filename), "wb") as file:
             while True:
-                pack = connectionSocket.recv(516)
-                ack_pack=packets.ACK_packet(pack[2:4])
-                connectionSocket.send(ack_pack)
-                if not pack:
+                pack = connectionSocket.recv(524)
+                ack_pack = packets.ACK_packet(pack[10:12])
+                dgram = packets.datagramHeader(12000, 12001, ack_pack)                
+                connectionSocket.send(dgram)
+                file.write(pack[12:])
+                if len(pack[12:])<512:
                     break
-                file.write(pack[4:])
-            
                 
+                
+
         file.close()
-        
+
     connectionSocket.close()
--- TFTPclient.py
+++ TFTPclient.py
@@ -1,17 +1,14 @@
 #****************************************************************
-# 
-#
-#
-#
-#
+# TFTPClient.py
+# Author: Galena Wilson
+# Purpose:
 #****************************************************************
 from socket import *
 import argparse
 import os
 import ntpath
-import sys
-import pickle
 import packets
+from pathlib import Path
 
 def to_int(binary):
     return int.from_bytes(binary, byteorder = "big")
@@ -19,11 +16,12 @@ def to_int(binary):
 def get_fname(file):
     head, tail = ntpath.split(file)
     return tail or ntpath.basename(head)   
-    
+        
 
-parser = argparse.ArgumentParser(description= 'Enter IP address, Server request ("rd" for read and "wrt" for write.)')
+parser = argparse.ArgumentParser(description = 'Enter IP address, Server request ("rd" for read and "wrt" for write.)')
 
-#parser.add_argument('hostname', type = str, help = 'IP address of destination')
+parser.add_argument('host port', type = str, help = 'IP address of destination')
+parser.add_argument('destination port', type = str, help = 'IP address of destination')
 parser.add_argument('request', type = str, help = 'request to server')
 parser.add_argument('filename', type = str, help = 'the file to be sent or received')
 args = parser.parse_args()
@@ -32,63 +30,71 @@ args = parser.parse_args()
 request = args.request
 filename = args.filename
 
-
    
-serverPort = 12000
+serverPort = 12018
 serverName = "127.0.0.1"
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
 
 #initiate read request
-    
+   
 if request == "rd":
     #To do: implement read request
     
     # send request to read
-    packet = packets.RD_WR_packet(request, filename)
-    clientSocket.send(packet)
-    
+    rd_packet = packets.RD_WR_packet(request, filename)
+    dgram = packets.datagramHeader(12001, 12000, rd_packet)
+    clientSocket.send(dgram)
+    filename = "copy_" + filename
     # receive data package
+    
     with open(get_fname(filename), "wb") as file:
     
         while True:
-            pack = clientSocket.recv(516)
-            ack_pack=packets.ACK_packet(pack[2:4])
-            clientSocket.send(ack_pack)
-            
-            if not pack:
+            pack = clientSocket.recv(524)
+            ack_pack=packets.ACK_packet(pack[10:12])
+            dgram = packets.datagramHeader(12000, 12001, ack_pack)                
+            clientSocket.send(dgram)
+            file.write(pack[12:])
+            if len(pack[12:]) < 512:
                 break
-            file.write(pack[4:])
-         
+           
         file.close()
         
+        
+    print("File received.")    
     # send ACK
     # if # of blocks is not next in the row: send error
     
 if request == "wrt":   
+    
     wrt_packet = packets.RD_WR_packet(request, filename)
-    clientSocket.send(wrt_packet)
+    dgram = packets.datagramHeader(12001, 12000, wrt_packet)
+    clientSocket.send(dgram)
     
     #read data from file
+    #if filename.exit
     with open(filename, 'rb') as file:
-    
+        
         # send data package with block # 1
         file_s = os.path.getsize(filename)
         end_file = round(file_s/512)
+        print(end_file)
         
         if end_file == 0:
             end_file= 1
         
         #ack = clientSocket.recv(4) # ack with block number 0
-        ack = clientSocket.recv(4)
-        if to_int(ack[0:2]) == 4:
+        ack = clientSocket.recv(12)
+        if to_int(ack[8:10]) == 4:
             for i in range (1, end_file+1):             # if the previous packet is ackn
-                if to_int(ack[2:]) == i-1:
+                if to_int(ack[10:]) == i-1:
                     data = file.read(512)               # read data
                     pack = packets.DATA_packet(data, i) # create data pack
-                    clientSocket.send(pack)   
-                ack = clientSocket.recv(4)        
-        
+                    dgram = packets.datagramHeader(12001, 12000, pack)
+                    clientSocket.send(dgram)   
+                ack = clientSocket.recv(12)        
+        print("File sent.")
         file.close()
         
 clientSocket.close()
